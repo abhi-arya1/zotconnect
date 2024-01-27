90024069fa7c2c83772f4980bbeac53158b52de4
Hrishikesh Srihari <rishisrihari9@gmail.com>
2024-01-26 20:59:07 -0800

    added typewriter and landing page text

--- app/(marketing)/_components/navbar.tsx
+++ app/(marketing)/_components/navbar.tsx
@@ -6,7 +6,9 @@ import { ModeToggle } from "@/components/mode_toggle";
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
 import { useConvexAuth } from "convex/react";
-import { SignInButton, UserButton } from "@clerk/clerk-react";
+import { SignInButton, UserButton, SignUpButton } from "@clerk/clerk-react";
+import Image from "next/image";
+
 
 export const Navbar = () => {
     const { isAuthenticated, isLoading } = useConvexAuth()
@@ -17,6 +19,15 @@ export const Navbar = () => {
             "z-50 bg-background dark:bg-[#1F1F1F] fixed top-0 flex items-center w-full p-6",
             scrolled && "border-b shadow-sm"
         )}>
+            <Link href="/signup">
+                <Image 
+                    src="/zotconnect.jpg" // Your image path
+                    alt="Logo"
+                    width={50} // Adjust as needed
+                    height={50} // Adjust as needed
+                    style={{ borderRadius: '10px', cursor: 'pointer' }} // Rounded edges and cursor style
+                />
+            </Link>
             <div className="md:ml-auto md:justify-end justify-between w-full flex items-center gap-x-2">
                 {isLoading && (
                     <div>Loading...</div>
@@ -28,17 +39,17 @@ export const Navbar = () => {
                                 Log In
                             </Button>
                         </SignInButton>
-                        <SignInButton mode="modal">
+                        <SignUpButton mode="modal">
                             <Button size="sm">
                                 Join ZotConnect
                             </Button>
-                        </SignInButton>
+                        </SignUpButton>
                     </>
                 )}
                 {isAuthenticated && !isLoading && (
                     <>
                     <Button size="sm" asChild>
-                        <Link href="/documents">
+                        <Link href="/signup">
                             Enter ZotConnect
                         </Link>
                     </Button>
--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -1,18 +1,41 @@
+"use client";
+
 import { Navbar } from './_components/navbar';
+import Typewriter from 'typewriter-effect';
 
 
 const LandingPage = () => {
   return (
     <div className="h-full flex flex-col">
       <Navbar />
-      <div className="h-full flex flex-col items-center justify-center
-        md:justify-start text-center gap-y-8 flex-1 px-6 pb-10">
-        <h1 className='text-4xl'>
-          Hello
-        </h1>
+      <img src="/zotconnect.png" className="mb-4 " />
+      <h1 className='text-4xl md:text-6xl font-bold mb-8 text-center justify-center h-screen'> Welcome to <span style={{ color: '#1cb2d4', fontStyle: 'italic' }}>ZotConnect</span></h1>
+
+      <div className='typewriter-container'>
+        <Typewriter
+            onInit={(typewriter) => {
+                typewriter.typeString('Connect with <span class="highlight">professors</span>.')
+                    .pauseFor(700)
+                    .changeDeleteSpeed(35)
+                    .deleteChars(11) // Adjust the number based on how many characters you want to remove
+                    .typeString('<span class="highlight">other students</span>.')
+                    .pauseFor(700)
+                    .deleteChars(15) // Adjust the number based on how many characters you want to remove
+                    .typeString('<span class="highlight">UCI alumni</span>.')
+                    .start()
+                    .pauseFor(700)
+                    .deleteChars(11) // Adjust the number based on how many characters you want to remove
+                    .typeString('<span class="highlight">companies</span>.')
+                    .pauseFor(700)
+                    .deleteChars(23)// Adjust the number based on how many characters you want to remove
+                
+                typewriter.typeString('Get your resume vetted by AI')
+            }}
+        />
       </div>
+      
     </div>
-  );
+);
 };
 
 export default LandingPage;
