2c6033e1cd0a4680d9c85493b48869d9a646786b
Abhi Arya <abhia9167@gmail.com>
2024-01-27 11:43:18 -0800

    added random fun fact

--- app/posts/layout.tsx
+++ app/posts/layout.tsx
@@ -2,6 +2,17 @@
 import { Spinner } from "@/components/spinner";
 import { useConvexAuth } from "convex/react";
 import { redirect } from "next/navigation";
+import funFactsData from '@/uci_fun_facts.json';
+
+type FunFact = {
+    fact: string;
+};
+
+const getRandomFunFact = (): FunFact => {
+    const funFacts = funFactsData.funFacts;
+    const randomIndex = Math.floor(Math.random() * funFacts.length);
+    return funFacts[randomIndex];
+};
 
 const PostsLayout = ({
     children
@@ -11,8 +22,13 @@ const PostsLayout = ({
     const { isAuthenticated, isLoading } = useConvexAuth(); 
 
     if (isLoading) {
+        const funFact = getRandomFunFact();
         return (
             <div className="h-full flex items-center justify-center dark:bg-[#1F1F1F]">
+                <div>
+                    <h2 className="text-[#2563eb] dark:text-[#0390fc]">Did You Know?</h2>
+                    <p>{funFact.fact}</p>
+                </div>
                 <Spinner size="lg"/>
             </div>
         )
