7291d0396b9a0f49637944e1770cd4767f3c522d
Hrishikesh Srihari <rishisrihari9@gmail.com>
2024-01-26 22:36:02 -0800

    fixed gradient art

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -8,13 +8,20 @@ const LandingPage = () => {
   return (
     <div className="h-full flex flex-col ">
       <Navbar />
-      <div>
-        <div className="absolute w-64 h-64 bg-gradient-to-r from-blue-500 to-purple-500 rounded-full blur-lg -top-50 left-50 z-0"></div>
+      <div className="flex items-center justify-center h-screen relative">
+        {/* Image */}
+        <img src="/zotconnect.png" className="max-w-[40%] mb-4 z-10"/>
+
+        {/* Circle with Gradient */}
+        <div style={{ width: '400px', height: '400px' }} className="absolute bg-gradient-to-r from-blue-700 to-purple-500 rounded-full blur-xl opacity-70 -bottom-20px left-1/2 transform -translate-x-1/2 z-0"></div>
+      </div>
+      {/* <div>
+        <div className="absolute w-64 h-64 bg-gradient-to-r from-blue-500 to-pink-500 -right-50 -left-40 rounded-full blur-lg mt-4"></div>
       </div>
       <div className='flex items-center justify-center h-screen'>
         <img src="/zotconnect.png" className="max-w-[40%] mb-4"/>
 
-      </div>
+      </div> */}
       
       <div className='text-4xl md:text-6xl font-bold items-center text-center justify-center h-screen mb-2'> Welcome to <span style={{ color: '#1cb2d4', fontStyle: 'italic' }}>ZotConnect</span><span style={{fontStyle: 'italic'}}>!</span></div>
 
