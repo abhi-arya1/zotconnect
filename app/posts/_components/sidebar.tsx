43fd84b42cbc6e66fe173604f84a7b705361d5d2
Hrishikesh Srihari <rishisrihari9@gmail.com>
2024-01-27 13:55:04 -0800

    tweaked sidebar animations

--- app/posts/_components/sidebar.tsx
+++ app/posts/_components/sidebar.tsx
@@ -9,6 +9,17 @@ const Sidebar: React.FC = () => {
     const router = useRouter();
     const { user } = useUser();
     const [isHovered, setIsHovered] = useState(false);
+
+    const sidebarStyle = {
+        transform: isHovered ? 'translateX(-20%)' : 'translateX(0)',
+        transition: 'transform 0.5s ease-in-out',
+      };
+    
+      const iconTextStyle = {
+        transform: isHovered ? 'translateX(0)' : 'translateX(-100%)',
+        transition: 'transform 0.5s ease-in-out',
+      };
+
     return (
         <div className="flex flex-col items-center justify-between h-screen w-20 p-15 transition-all duration-500 ease-in-out bg-gray-300 hover:w-52 dark:bg-neutral-700 rounded-r-3xl shadow-xl" 
             onMouseEnter={() => setIsHovered(true)}
@@ -24,16 +35,20 @@ const Sidebar: React.FC = () => {
                     className="drop-shadow-2xl pt-8"
                 />
             </Button>
-            <div className={`flex flex-col items-start transition-transform duration-500 ease-in-out transform ${isHovered ? 'translate-x-[-20%]' : 'translate-x-0'}`}>
-                <div className="flex items-center group">
-                    <SidebarClose className="pt-2 h-[70px] transition-transform duration-500 ease-in-out group-hover:translate-x-[-10px]" />
-                    <span className='ml-6 font-bold transition-all duration-500 ease-in-out transform translate-x-[-100%] group-hover:translate-x-0'>Close</span>
-                </div>
-                <div className="flex items-center group">
-                    <Pencil className="pt-2 h-[70px] transition-transform duration-500 ease-in-out group-hover:translate-x-[-10px]" />
-                    <span className='ml-6 font-bold transition-all duration-500 ease-in-out transform translate-x-[-100%] group-hover:translate-x-0'>Edit</span>
-                </div>
+            <div className="flex items-center">
+                <SidebarClose className="pt-2 h-[70px] translate-x-[-10px]" />
+                <span className="ml-6 font-bold" style={iconTextStyle}>
+                Close
+                </span>
             </div>
+            <div className="flex items-center">
+                <Pencil className="pt-2 h-[70px] translate-x-[-10px]" />
+                <span className="ml-6 font-bold" style={iconTextStyle}>
+                Edit
+                </span>
+            </div>
+
+
 
 
 
