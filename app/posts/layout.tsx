5d19e7489745c5ac9ed2c0fe3a5095fd5c5df8c8
Hrishikesh Srihari <rishisrihari9@gmail.com>
2024-01-27 11:44:57 -0800

    added sidebar

--- app/posts/layout.tsx
+++ app/posts/layout.tsx
@@ -1,6 +1,7 @@
 "use client";
 import { Spinner } from "@/components/spinner";
 import { useConvexAuth } from "convex/react";
+import { Sidebar } from "lucide-react";
 import { redirect } from "next/navigation";
 import funFactsData from '@/uci_fun_facts.json';
 
@@ -15,36 +16,31 @@ const getRandomFunFact = (): FunFact => {
 };
 
 const PostsLayout = ({
-    children
+  children
 }: {
-    children: React.ReactNode; 
+  children: React.ReactNode;
 }) => {
-    const { isAuthenticated, isLoading } = useConvexAuth(); 
+  const { isAuthenticated, isLoading } = useConvexAuth();
 
-    if (isLoading) {
-        const funFact = getRandomFunFact();
-        return (
-            <div className="h-full flex items-center justify-center dark:bg-[#1F1F1F]">
-                <div>
-                    <h2 className="text-[#2563eb] dark:text-[#0390fc]">Did You Know?</h2>
-                    <p>{funFact.fact}</p>
-                </div>
-                <Spinner size="lg"/>
-            </div>
-        )
-    }
-
-    if (!isAuthenticated) {
-        return redirect("/"); 
-    }
-
-    return ( 
-        <div className="h-full flex dark:bg-[#1F1F1F]">
-            <main className="flex-1 h-full overflow-y-auto">
-                {children}
-            </main>
-        </div>
+  if (isLoading) {
+    return (
+      <div className="h-full flex items-center justify-center dark:bg-[#1F1F1F]">
+        <Spinner size="lg" />
+      </div>
     );
-}
- 
+  }
+
+  if (!isAuthenticated) {
+    return redirect("/");
+  }
+
+  return (
+    <div className="h-full flex">
+      <main className="flex-1 h-full overflow-y-auto dark:bg-[#1F1F1F]">
+        {children}
+      </main>
+    </div>
+  );
+};
+
 export default PostsLayout;
