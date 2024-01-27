8ab86eb7e734646114274b1e4846b8f4dc826337
Abhi Arya <abhia9167@gmail.com>
2024-01-26 21:40:08 -0800

    added prof features for signup

---
+++ app/signup/_components/input_interest.tsx
@@ -0,0 +1,38 @@
+import React, { useState } from 'react';
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+
+interface InputWithButtonProps {
+  onInputSubmit: (type: string, value: string) => void;
+  placeholder: string; // Adding the placeholder prop
+}
+
+export function InputWithButton({ onInputSubmit, placeholder }: InputWithButtonProps) {
+  const [inputValue, setInputValue] = useState<string>('');
+
+  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setInputValue(event.target.value);
+  };
+
+  const handleSubmit = () => {
+    if (onInputSubmit) {
+      if(placeholder === "Major (No Abbreviations)") {
+        onInputSubmit("Major", inputValue);
+      } else {
+        onInputSubmit("Interest/Field", inputValue);
+      }
+    }
+  };
+
+  return (
+    <div className="flex w-[300px] items-center space-x-2">
+      <Input
+        type="text"
+        placeholder={placeholder} // Using the placeholder prop
+        value={inputValue}
+        onChange={handleInputChange}
+      />
+      <Button type="submit" onClick={handleSubmit}>Save</Button>
+    </div>
+  );
+}
--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -6,6 +6,7 @@ import { Button } from "@/components/ui/button";
 import { TextareaForm } from "./_components/bio_form";
 import * as z from "zod"
 import { toast } from "@/components/ui/use-toast";
+import { InputWithButton } from "./_components/input_interest";
 
 const FormSchema = z.object({
     bio: z
@@ -19,11 +20,17 @@ const FormSchema = z.object({
   })
 
 const SignupPages = () => {
-    const [userType, setUserType] = useState('STUDENT')
+    // USER DATA
+    const { user } = useUser(); 
+    const userId = user?.id;
+    const [userType, setUserType] = useState("");
+    const [bio, setBio] = useState("");
+    const [majorInterest, setMajorInterest] = useState("")
+
+
+    // PAGE STATES 
     const [studentButtonDisable, setStudentButtonDisable] = useState(false)
     const [profButtonDisable, setProfButtonDisable] = useState(false)
-    const [bio, setBio] = useState("")
-    const { user } = useUser(); 
 
     const selectedStudent = () => {
         setUserType("STUDENT")
@@ -37,11 +44,18 @@ const SignupPages = () => {
         setStudentButtonDisable(true)
     }
 
-    const handleFormSubmit = (formData: z.infer<typeof FormSchema>) => {
+    const handleBioFormSubmit = (formData: z.infer<typeof FormSchema>) => {
         toast({
             title: "Saved Bio!"
           })
        setBio(formData.bio)
+    };
+
+    const handleMajorInterestSubmit = (type: string, value: string) => {
+        toast({
+            title: `Saved ${type} as ${value}!`
+          })
+        setMajorInterest(value)
     };
 
     return ( 
@@ -50,6 +64,8 @@ const SignupPages = () => {
             <div className="pt-40 font-bold text-3xl">
                 Welcome to ZotConnect, <span className="text-[#03fcf8]">{user?.fullName}</span>!
             </div>
+
+            {/* BASIC INFORMATION */}
             <div className="flex flex-col pt-10">
                 <Button variant={studentButtonDisable ? "ghost" : "default"} onClick={selectedStudent}>
                     Student
@@ -61,8 +77,29 @@ const SignupPages = () => {
                 </div>
             </div>
             <div className="flex flex-col justify-center items-center">
-                <TextareaForm onFormSubmit={handleFormSubmit}/>
+                <TextareaForm onFormSubmit={handleBioFormSubmit}/>
             </div>
+            <div className="pt-10">
+                {userType === "STUDENT" ? (
+                    <div>
+                        <InputWithButton
+                            onInputSubmit={handleMajorInterestSubmit}
+                            placeholder="Major (No Abbreviations)"
+                        />
+                    </div>
+                ) : userType === "PROF" ? (
+                    <div>
+                        <InputWithButton
+                            onInputSubmit={handleMajorInterestSubmit}
+                            placeholder="Research Interest or Field"
+                        />
+                    </div>
+                ) : (
+                    <div>Please Select A Role, either <span className="text-[#03fcf8]">Student</span> or <span className="text-[#03fcf8]">Professor</span></div>
+                )}
+            </div>
+
+
         </div>
     </div> 
     );
