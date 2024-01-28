01ec4a842d46d71b990783ad3488f63f95dffd19
Hrishikesh Srihari <rishisrihari9@gmail.com>
2024-01-27 16:19:07 -0800

    changed sidebar dynamic

--- app/posts/_components/sidebar.tsx
+++ app/posts/_components/sidebar.tsx
@@ -8,23 +8,10 @@ import { UserButton, useUser } from '@clerk/clerk-react';
 const Sidebar: React.FC = () => {
     const router = useRouter();
     const { user } = useUser();
-    const [isHovered, setIsHovered] = useState(false);
-
-    const sidebarStyle = {
-        transform: isHovered ? 'translateX(-20%)' : 'translateX(0)',
-        transition: 'transform 0.5s ease-in-out',
-      };
-    
-      const iconTextStyle = {
-        transform: isHovered ? 'translateX(0)' : 'translateX(-100%)',
-        transition: 'transform 0.5s ease-in-out',
-      };
 
     return (
         <div className="fixed left-0 top-0">
-        <div className="flex flex-col items-center justify-between h-screen w-20 p-15 transition-all duration-500 ease-in-out bg-gray-300 hover:w-52 dark:bg-neutral-700 rounded-r-3xl shadow-xl" 
-            onMouseEnter={() => setIsHovered(true)}
-            onMouseLeave={() => setIsHovered(false)}
+        <div className="flex flex-col items-center justify-between h-screen w-20 p-15 bg-gray-300 dark:bg-neutral-700 rounded-r-3xl shadow-xl" 
         >
             <Button variant='ghost' className="hover:bg-gray-300 dark:hover:bg-neutral-700 hover:cursor-pointer" onClick={() => {router.push('/posts')}}>
                 <Image 
@@ -36,11 +23,15 @@ const Sidebar: React.FC = () => {
                     className="drop-shadow-2xl pt-8"
                 />
             </Button>
-            <div className="flex items-center">
-                <SidebarClose className="pt-2 h-[70px] translate-x-[-10px]" />
+            <div className="flex items-center justify-center">
+                <Button variant='ghost' className="hover:bg-gray-300 dark:hover:bg-neutral-700 hover:cursor-pointer" onClick={() => {router.push('/')}}>
+                    <SidebarClose className="pt-2" />
+                </Button>
             </div>
             <div className="flex items-center">
-                <Pencil className="pt-2 h-[70px] translate-x-[-10px]" />
+                <Button variant='ghost' className="hover:bg-gray-300 dark:hover:bg-neutral-700 hover:cursor-pointer" onClick={() => {router.push('/new_post')}}>
+                    <Pencil className="pt-2" />
+                </Button>
             </div>
             <div className="pb-8">
                 <UserButton />
